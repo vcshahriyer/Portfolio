7e12e9ed2cac03835dc51590a49db2be755fc072
vcshahriyer <shahriyer99@gmail.com>
2017-09-28 00:32:18 +0600

    First section with media query

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    
+var typed = new Typed('.typejs', {
+strings: ["Shahriyer ^1000","a full stack developer ^2000"],
+typeSpeed: 70,
+backSpeed: 30,
+smartBackspace: true,
+loop: true
+});
+
+});
