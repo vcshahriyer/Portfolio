8408d99f686b4cc2b80a0d6361d51eebecdecb9f
vcshahriyer <shahriyer99@gmail.com>
2017-09-29 01:11:48 +0600

    Mobile bug fix11

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     
     
 var typed = new Typed('.typejs', {
-strings: ["Shahriyer ^1000","a full stack developer ^2000"],
-typeSpeed: 70,
+strings: ["Shahriyer ^3500","a full stack developer ^3000"],
+typeSpeed: 60,
 backSpeed: 30,
 smartBackspace: true,
 loop: true
