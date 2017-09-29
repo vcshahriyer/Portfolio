0c587f094b4468bd05b27db29b0698a5f0f890b2
vcshahriyer <shahriyer99@gmail.com>
2017-09-29 22:58:44 +0600

    Preloader added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+//    ===== My preloader sugnature =======
+    var svg = new Walkway({
+        selector:"#Layer_1",
+        duration: 3000,
+        easing: 'easeInOutCubic'
+    }).draw();
+    $('.svg-img').fadeIn(100);
+    $('.preloader').delay(3000).fadeOut(400);
     
  var typed3 = new Typed('.typejs', {
     strings: [' <span class="tp_color">A designer</span>^1000', '<span class="tp_color">A developer</span>^1000', '<span class="tp_color">A coder</span>^1000'],
