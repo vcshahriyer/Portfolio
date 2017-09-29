da60a73c98b401f901814a6fa6c43d14c6d9f36b
vcshahriyer <shahriyer99@gmail.com>
2017-09-29 11:24:55 +0600

    Header final relese

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
     
     
-var typed = new Typed('.typejs', {
-strings: ["Shahriyer ^3500","a full stack developer ^3000"],
-typeSpeed: 60,
-backSpeed: 30,
-smartBackspace: true,
-loop: true
-});
+//var typed = new Typed('.typejs', {
+//strings: ["Shahriyer ^3500","a full stack developer ^3000"],
+//typeSpeed: 60,
+//backSpeed: 30,
+//smartBackspace: true,
+//loop: true
+//});
+	
+	
+	
+ var typed3 = new Typed('.typejs', {
+    strings: [' <span class="tp_color">A designer</span>^1000', '<span class="tp_color">A developer</span>^1000', '<span class="tp_color">A coder</span>^1000'],
+    typeSpeed: 40,
+    backSpeed: 0,
+//    smartBackspace: true, // this is a default
+    loop: true
+  });
 
 });
