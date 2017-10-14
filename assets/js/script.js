f8badd20984babf771fb64e80f7d3a9530093de6
vcshahriyer <shahriyer99@gmail.com>
2017-10-15 01:24:47 +0600

    All SVG animation fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 //    ===== My preloader sugnature =======
     var svg = new Walkway({
         selector:"#Layer_1",
-        duration: 3000,
+        duration: 2000,
         easing: 'easeInOutCubic'
     }).draw();
     $('.svg-img').fadeIn(100);
-    $('.preloader').delay(3000).fadeOut(400);
+    $('.preloader').delay(2000).fadeOut(400);
     
  var typed3 = new Typed('.typejs', {
-    strings: [' <span class="tp_color">A designer</span>^1000', '<span class="tp_color">A developer</span>^1000', '<span class="tp_color">A coder</span>^1000'],
+    strings: [' <span class="tp_color">A Designer</span>^2000', '<span class="tp_color">A Developer</span>^2000', '<span class="tp_color">A Debugger</span>^2000'],
     typeSpeed: 40,
     backSpeed: 0,
     smartBackspace: false, // this is a default
