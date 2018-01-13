81568f08e256860099bcfb1647e5038d928198f3
vcshahriyer <shahriyer99@gmail.com>
2018-01-13 22:36:53 +0600

    Load time reduce-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         easing: 'easeInOutCubic'
     }).draw();
     $('.svg-img').fadeIn(100);
-    $('.preloader').delay(2000).fadeOut(400);
+    $('.preloader').delay(800).fadeOut(400);
 
  var typed3 = new Typed('.typejs', {
     strings: [' <span class="tp_color">A Designer</span>^2000', '<span class="tp_color">Developer</span>^2000', '<span class="tp_color">Debugger</span>^2000'],
